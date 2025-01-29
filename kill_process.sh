de373b5226d6b85f96c61d65be6fb53c64290d0e
anonymoustest02 <sufssl02@gmail.com>
2025-01-29 13:26:10 -0600

    Update kill_process.sh

--- kill_process.sh
+++ kill_process.sh
@@ -2,17 +2,32 @@
 
 # Find the process ID (PID) of the 'python3 sleep.py' process
 # PID=$(ps aux | grep "[p]ython3 sleep.py" | awk '{print $2}')
-PID=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
+# PID=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
 
-if [ -z "$PID" ]; then
-    echo "Process 'python3 sleep.py' not found."
+# if [ -z "$PID" ]; then
+#     echo "Process 'python3 sleep.py' not found."
+# else
+#     echo "Found process 'python3 sleep.py' with PID: $PID"
+#     echo "Killing the process..."
+#     kill "$PID"
+#     if [ $? -eq 0 ]; then
+#         echo "Process killed successfully."
+#     else
+#         echo "Failed to kill the process."
+#     fi
+# fi
+
+PIDS=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
+
+if [ -z "$PIDS" ]; then
+    echo "Process 'run.sh' not found."
 else
-    echo "Found process 'python3 sleep.py' with PID: $PID"
-    echo "Killing the process..."
-    kill "$PID"
+    echo "Found process 'run.sh' with PIDs: $PIDS"
+    echo "Killing the processes..."
+    kill $PIDS
     if [ $? -eq 0 ]; then
-        echo "Process killed successfully."
+        echo "Processes killed successfully."
     else
-        echo "Failed to kill the process."
+        echo "Failed to kill some processes."
     fi
 fi
