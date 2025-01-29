2c26c1119632f5f8dea894125d561a16378566c2
anonymoustest02 <sufssl02@gmail.com>
2025-01-29 15:43:33 -0600

    Update kill_process.sh

--- kill_process.sh
+++ kill_process.sh
@@ -3,19 +3,20 @@
 # Find the process ID (PID) of the 'python3 sleep.py' process
 # PID=$(ps aux | grep "[p]ython3 sleep.py" | awk '{print $2}')
 # PID=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
+PID=$(pgrep -f -o run_123.sh)
 
-# if [ -z "$PID" ]; then
-#     echo "Process 'python3 sleep.py' not found."
-# else 
-#     echo "Found process 'python3 sleep.py' with PID: $PID"
-#     echo "Killing the process..."
-#     kill "$PID"
-#     if [ $? -eq 0 ]; then 
-#         echo "Process killed successfully."
-#     else
-#         echo "Failed to kill the process."
-#     fi
-# fi
+if [ -z "$PID" ]; then
+    echo "Process 'run.sh' not found."
+else 
+    echo "Found process 'run.sh' with PID: $PID"
+    echo "Killing the process..."
+    pkill -TERM -g "$PID"
+    if [ $? -eq 0 ]; then 
+        echo "Process killed successfully."
+    else
+        echo "Failed to kill the process."
+    fi
+fi
 
 # PIDS=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
  
@@ -32,4 +33,4 @@
 #     fi
 # fi
  
-ps aux | grep "[\./]run.sh" | awk '{print $2}' | xargs -r kill
+# ps aux | grep "[\./]run.sh" | awk '{print $2}' | xargs -r kill
