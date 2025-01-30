6ee5273aa006a08adef253a5556998694762d5e2
anonymoustest02 <sufssl02@gmail.com>
2025-01-30 13:32:19 -0600

    Update kill_process.sh

--- kill_process.sh
+++ kill_process.sh
@@ -3,8 +3,8 @@
 # Find the process ID (PID) of the 'python3 sleep.py' process
 # PID=$(ps aux | grep "[p]ython3 sleep.py" | awk '{print $2}')
 # PID=$(ps aux | grep "[\./]run.sh" | awk '{print $2}')
-PID=$(pgrep -f -o run.sh) 
-# PID=$(pgrep -f -o Runner.Listener)
+# PID=$(pgrep -f -o run.sh) 
+PID=$(pgrep -f -o Runner.Listener)
  
 if [ -z "$PID" ]; then
     echo "Process 'run.sh' not found." 
